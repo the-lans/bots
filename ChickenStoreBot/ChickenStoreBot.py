4c6fb65770ace3bcaa3ad1940076a4e280b2f5f9
thelans <ilyav87@gmail.com>
2019-07-26 10:17:10 +0300

    Update

--- ChickenStoreBot/ChickenStoreBot.py
+++ ChickenStoreBot/ChickenStoreBot.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, Response
+
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
-from viberbot.api.messages import VideoMessage
 from viberbot.api.messages.text_message import TextMessage
-import logging
 
 from viberbot.api.viber_requests import ViberConversationStartedRequest
 from viberbot.api.viber_requests import ViberFailedRequest
@@ -11,11 +10,23 @@ from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberSubscribedRequest
 from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
+import time
+import logging
+import sched
+import threading
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 app = Flask(__name__)
 viber = Api(BotConfiguration(
-    name='PythonSampleBot',
-    avatar='http://site.com/avatar.jpg',
-    auth_token='445da6az1s345z78-dazcczb2542zv51a-e0vc5fva17480im9'
+    name='ChickenStoreBot',
+    avatar='https://iplaces.ru/AvatarChicken.jpg',
+    auth_token='49fe7d423d67d422-31dda79e9d6248e7-300c2231ba176c71'
 ))
 
 @app.route('/deploy', methods=['POST'])
@@ -36,13 +47,25 @@ def incoming():
         ])
     elif isinstance(viber_request, ViberSubscribedRequest):
         viber.send_messages(viber_request.get_user.id, [
-            TextMessage(text="thanks for subscribing!")
+            TextMessage(text="Спасибо за подписку!")
         ])
     elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+        logger.warning("Клиент не получил сообщение. Отказ: {0}".format(viber_request))
 
     return Response(status=200)
 
+def set_webhook(viber):
+    event_types = viber.set_webhook('https://iplaces.ru:443/deploy')
+    print("Webhook")
+    print(event_types)
+
 if __name__ == "__main__":
+    print("Bot run...")
+    scheduler = sched.scheduler(time.time, time.sleep)
+    scheduler.enter(5, 1, set_webhook, (viber,))
+    t = threading.Thread(target=scheduler.run)
+    t.start()
+
     context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
+    app.run(host='176.57.216.209', port=443, debug=True, ssl_context=context)
+    print("End bot!")
